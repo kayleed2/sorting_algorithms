a1213f03c5884f7843d305bf3662e456d53b3a08
Kaylee Dao <3597@holbertonschool.com>
2021-12-21 15:23:08 -0800

    Partial Insertion Sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,27 +8,19 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current = *list;
-	listint_t *tmp = NULL;
+	listint_t *current, listint_t *tmp;
 	listint_t *copy = NULL;
 
 	if (!list)
 		return;
-	
+
+	current = (*list)->next;
+	tmp = current;
+
 	while (current != NULL)
 	{
-		tmp = current->next;
-		while (tmp && tmp->prev &&
-				tmp->n < tmp->prev->n)
+		if (tmp->n < tmp->prev->n)
 		{
-			copy = current;
-			current = tmp;
-			tmp = copy;
-			print_list(*list);
-			tmp = tmp->prev;
+			
 		}
-		current = current->next;
-		print_list(*list);
-		current++;
-	}
 }

ce9a10cb7c459133fbe11020a2e4f914739dc2c8
Kaylee Dao <kayleed2@gmail.com>
2022-05-04 11:11:43 -0500

    Adding new files for sorting algorithm

---
+++ 103-merge_sort.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "sort.h"
+
+/**
+* merge_sort - Sorts array in ascending order
+* array: array to be sorted
+* size: size of array
+* Return: Sorted array
+**/
+
+void merge_sort(int *array, size_t size);
+{
+    
+}

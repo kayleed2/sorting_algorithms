b7f49011db0ecac3956de121025132146ba64c9b
Kaylee Dao <kayleed2@gmail.com>
2022-05-05 10:11:51 -0500

    Checking merge sort

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -10,29 +10,29 @@
 
 void merge_sort(int *array, size_t size)
 {
-    // int *right;
-    // int *left;
+    int *right;
+    int *left;
     int mid;
-    // int start;
-    // int leftPointer;
-    // int rightPointer;
+    int start;
+    int leftPointer;
+    int rightPointer;
 
     if (size < 2)
         return;
 
     if (size % 2 != 0)
         mid = size - (size / 2);
+    else 
+        mid = size / 2;
     
-    printf("%ul", (unsigned long) size);
-    
-    // mid = size / 2;
-    // right = array + (mid + 1);
-    // left
+    printf("HELLO I AM %d", mid);
+    right = array + (mid + 1);
+    left = right - (mid + 1);
 
-    // merge_sort(right, (size - mid));
-    // merge_sort(left, mid);
+    merge_sort(right, (size - mid));
+    merge_sort(left, mid);
     
-    // start = 0;
-    // leftPointer = start;
-    // rightPointer = 
+    start = 0;
+    leftPointer = start;
+    rightPointer = mid;
 }

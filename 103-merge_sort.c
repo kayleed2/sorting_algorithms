694e2b2b4a9ac24f314ee6de5dfadacceffd7ae9
Kaylee Dao <kayleed2@gmail.com>
2022-05-06 12:27:54 -0500

    No use for left and right arrays

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -10,9 +10,9 @@
 
 void merge_sort(int *array, size_t size)
 {
+    int l = 0;
+    int r = size;
     int mid;
-    int *final_array;
-    int i;
 
     if (size < 2)
         return;
@@ -22,27 +22,13 @@ void merge_sort(int *array, size_t size)
     else 
         mid = size / 2;
 
-    final_array = malloc(sizeof(array));
+    if (l < r) 
+    {
+        merge_sort(array, mid);
+        merge_sort(array, mid + 1);
 
-    if (!final_array) 
-        free(final_array);
-
-    merge(0, mid, array, final_array);
-    merge(mid + 1, size, array, final_array);
-
-    for (i = 0; i <= (int)size; i++) {
-        array[i] = final_array[i];
+        merge(array, l, mid, size);
     }
-
-    printf("Left array: ");
-    for (i = 0; i < (int)size; i++)
-        printf("%d, ", array[i]);
-    
-    printf("\n");
-    printf("Right array: ");
-    
-    for (i = 0; i < (int)size; i++)
-        printf("%d, ", final_array[i]);
 }
 
 /**
@@ -53,27 +39,47 @@ void merge_sort(int *array, size_t size)
 * @final_array: final array
 * Return: Sorted array
 **/
-void merge(int start, int end, int *arr, int *final_array) 
+void merge(int *array, int l, int m, int r) 
 {
-    int leftPointer = start;
-    int rightPointer = ((start + end) / 2) + 1;
-    int size = start + end;
-    int mid = ((start + end) / 2);
-    int k;
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    int L[n1];
+    int R[n2];
 
-    for (k = start; k <= size; k++) {
-        if (leftPointer == mid + 1) {
-            final_array[k] = arr[rightPointer];
-            rightPointer++;
-        } else if (rightPointer == size + 1) {
-            final_array[k] = arr[leftPointer];
-            leftPointer++;
-        } else if (arr[leftPointer] < arr[rightPointer]) {
-            final_array[k] = arr[leftPointer];
-            leftPointer++;
-        } else {
-            final_array[k] = arr[rightPointer];
-            rightPointer++;
+    for (i = 0; i < n1; i++)
+        L[i] = array[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = array[m + 1 + j];
+
+    i = 0;
+    j = 0;
+    k = l;
+
+    while (i < n1 && j < n2) 
+    {
+        if (L[i] <= R[j]) {
+            array[k] = L[i];
+            i++;
         }
+        else {
+            array[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) 
+    {
+        array[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) 
+    {
+        array[k] = R[j];
+        j++;
+        k++;
     }
 }

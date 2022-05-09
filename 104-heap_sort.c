4ba08c080260e88213847372f0c4a30ee2be9285
Kaylee Dao <kayleed2@gmail.com>
2022-05-09 15:03:00 -0500

    Sorted, but incorrect output

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,15 +1,66 @@
 #include "sort.h"
 
 /**
-* merge - Sorts array
-* @l: starting point of array
-* @r: ending point of array
-* @m: mid point of array
-* @array: array to be sorted
-* @final_array: final array
-* Return: Sorted array
+* max - Sorts array
+* @a: array
+* @n: size of array
+* @i: index 
+* @j: val
+* @k: val
+* Return: max
 **/
 
-void heap_sort(int *array, size_t size) {
-    print_array(array, size);
+int max(int *a, int n, int i, int j, int k) {
+    int m = i;
+    if (j < n && a[j] > a[m]) {
+        m = j;
+    }
+    if (k < n && a[k] > a[m]) {
+        m = k;
+    }
+    return m;
+}
+
+/**
+* sift - Sifts down
+* @a: array
+* @n: size of array
+* @i: index
+* Return: Void
+**/
+
+void sift(int *a, int n, int i) 
+{
+    while (1) {
+        int j = max(a, n, i, 2 * i + 1, 2 * i + 2);
+        if (j == i) {
+            break;
+        }
+        int t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+        i = j;
+    }
+}
+
+/**
+* heap_sort - Sorts array
+* @array: array
+* @size: size of array
+* Return: Void
+**/
+
+void heap_sort(int *array, size_t size)
+{
+    int i;
+    
+    for (i = (size - 2) / 2; i >= 0; i--) {
+        sift(array, size, i);
+    }
+    for (i = 0; i < (int)size; i++) {
+        int t = array[size - i - 1];
+        array[size - i - 1] = array[0];
+        array[0] = t;
+        sift(array, size - i - 1, 0);
+    }
 }

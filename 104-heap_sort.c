c99db6f60eba63fb206601caec61c20257b8341d
Kaylee Dao <kayleed2@gmail.com>
2022-05-09 14:43:20 -0500

    Trial to check compilation

---
+++ 104-heap_sort.c
@@ -0,0 +1,15 @@
+#include "sort.h"
+
+/**
+* merge - Sorts array
+* @l: starting point of array
+* @r: ending point of array
+* @m: mid point of array
+* @array: array to be sorted
+* @final_array: final array
+* Return: Sorted array
+**/
+
+void heap_sort(int *array, size_t size) {
+    print_array(array, size);
+}

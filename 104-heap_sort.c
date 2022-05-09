80e76059408916480882faaa9688102213b96e3e
Kaylee Dao <kayleed2@gmail.com>
2022-05-09 15:03:55 -0500

    Sorted, but incorrect output

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -31,12 +31,14 @@ int max(int *a, int n, int i, int j, int k) {
 
 void sift(int *a, int n, int i) 
 {
+    int t;
+
     while (1) {
         int j = max(a, n, i, 2 * i + 1, 2 * i + 2);
         if (j == i) {
             break;
         }
-        int t = a[i];
+        t = a[i];
         a[i] = a[j];
         a[j] = t;
         i = j;
@@ -53,12 +55,13 @@ void sift(int *a, int n, int i)
 void heap_sort(int *array, size_t size)
 {
     int i;
+    int t;
     
     for (i = (size - 2) / 2; i >= 0; i--) {
         sift(array, size, i);
     }
     for (i = 0; i < (int)size; i++) {
-        int t = array[size - i - 1];
+        t = array[size - i - 1];
         array[size - i - 1] = array[0];
         array[0] = t;
         sift(array, size - i - 1, 0);

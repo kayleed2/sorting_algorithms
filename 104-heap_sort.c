0e7e259c15a8c70fd475eff98fb2bea44311327d
Kaylee Dao <kayleed2@gmail.com>
2022-05-09 15:50:50 -0500

    Betty

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -4,21 +4,25 @@
 * max - Sorts array
 * @a: array
 * @n: size of array
-* @i: index 
+* @i: index
 * @j: val
 * @k: val
 * Return: max
 **/
 
-int max(int *a, int n, int i, int j, int k) {
-    int m = i;
-    if (j < n && a[j] > a[m]) {
-        m = j;
-    }
-    if (k < n && a[k] > a[m]) {
-        m = k;
-    }
-    return m;
+int max(int *a, int n, int i, int j, int k)
+{
+int m = i;
+
+if (j < n && a[j] > a[m])
+{
+m = j;
+}
+if (k < n && a[k] > a[m])
+{
+m = k;
+}
+return (m);
 }
 
 /**
@@ -29,20 +33,23 @@ int max(int *a, int n, int i, int j, int k) {
 * Return: Void
 **/
 
-void sift(int *a, int n, int i) 
+void sift(int *a, int n, int i)
 {
-    int t;
+int t;
 
-    while (1) {
-        int j = max(a, n, i, 2 * i + 1, 2 * i + 2);
-        if (j == i) {
-            break;
-        }
-        t = a[i];
-        a[i] = a[j];
-        a[j] = t;
-        i = j;
-    }
+while (1)
+{
+int j = max(a, n, i, 2 * i + 1, 2 * i + 2);
+
+if (j == i)
+{
+break;
+}
+t = a[i];
+a[i] = a[j];
+a[j] = t;
+i = j;
+}
 }
 
 /**
@@ -54,18 +61,20 @@ void sift(int *a, int n, int i)
 
 void heap_sort(int *array, size_t size)
 {
-    int i;
-    int t;
-    
-    for (i = (size - 2) / 2; i >= 0; i--) {
-        sift(array, size, i);
-    }
-    for (i = 0; i < (int)size; i++) {
-        if (i != 0) 
-            print_array(array, size);
-        t = array[size - i - 1];
-        array[size - i - 1] = array[0];
-        array[0] = t;
-        sift(array, size - i - 1, 0);
-    }
+int i;
+int t;
+
+for (i = (size - 2) / 2; i >= 0; i--)
+{
+sift(array, size, i);
+}
+for (i = 0; i < (int)size; i++)
+{
+if (i != 0)
+print_array(array, size);
+t = array[size - i - 1];
+array[size - i - 1] = array[0];
+array[0] = t;
+sift(array, size - i - 1, 0);
+}
 }

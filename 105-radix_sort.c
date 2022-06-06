7fae593b16e46519b81ea312fa25c88849ceb5e6
Kaylee Dao <kayleed2@gmail.com>
2022-06-06 09:00:49 -0500

    Betty changes

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -3,17 +3,23 @@
  * get_max - get max element
  * @a: array
  * @n: size
+ * Return: int
  */
 
-int get_max (int *a, size_t n)
+int get_max(int *a, size_t n)
 {
-    size_t i;
-    int max = a[0];
-    
-    for (i = 1; i < n; i++)
-        if (a[i] > max)
-            max = a[i];
-    return max;
+size_t i;
+int max = a[0];
+
+for (i = 1; i < n; i++)
+{
+if (a[i] > max)
+{
+max = a[i];
+}
+
+}
+return (max);
 }
 
 /**
@@ -24,41 +30,43 @@ int get_max (int *a, size_t n)
  */
 void radix_sort(int *array, size_t size)
 {
-    size_t bucket[10][10], bucket_cnt[10];
-    size_t i, j, k, r, NOP = 0, divisor = 1, lar, pass;
-    lar = get_max (array, size);
-    while (lar > 0){
-        NOP++;
-        lar /= 10;
-    }
-    for (pass = 0; pass < NOP; pass++){
-        for (i = 0; i < 10; i++){
-            bucket_cnt[i] = 0;
-        }
-        for (i = 0; i < size; i++){
-            r = (array[i] / divisor) % 10;
-            bucket[r][bucket_cnt[r]] = array[i];
-            bucket_cnt[r] += 1;
-        }
-        i = 0;
-        for (k = 0; k < 10; k++){
-            for (j = 0; j < bucket_cnt[k]; j++){
-                array[i] = bucket[k][j];
-                i++;
-            }
-        }
-        divisor *= 10;
-        for (i = 0; i < size; i++)
-        {
-            if (i == size - 1)
-            {
-                printf ("%d", array[i]);
-            }
-            else 
-            {
-                printf ("%d, ", array[i]);
-            }
-        }
-        printf ("\n");
-    }
+size_t bucket[10][10], bucket_cnt[10];
+size_t i, j, k, r, NOP = 0, divisor = 1, lar, pass;
+lar = get_max (array, size);
+while (lar > 0)
+{
+NOP++;
+lar /= 10;
+}
+for (pass = 0; pass < NOP; pass++)
+{
+for (i = 0; i < 10; i++)
+{
+bucket_cnt[i] = 0;
+}
+for (i = 0; i < size; i++)
+{
+r = (array[i] / divisor) % 10;
+bucket[r][bucket_cnt[r]] = array[i];
+bucket_cnt[r] += 1;
+}
+i = 0;
+for (k = 0; k < 10; k++)
+{
+for (j = 0; j < bucket_cnt[k]; j++)
+{
+array[i] = bucket[k][j];
+i++;
+}
+}
+divisor *= 10;
+for (i = 0; i < size; i++)
+{
+if (i == size - 1)
+printf("%d", array[i]);
+else
+printf("%d, ", array[i]);
+}
+printf("\n");
+}
 }

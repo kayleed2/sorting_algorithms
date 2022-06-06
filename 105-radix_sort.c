09d8c82aed53627e15bead260aa1ab8ef3067146
Kaylee Dao <kayleed2@gmail.com>
2022-06-06 08:28:51 -0500

    Correct files

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -1,21 +1,49 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "sort.h"
-
 /**
- * main - Entry point
- *
- * Return: Always 0
+ * radix_sort - sorts an array of integers in ascending order
+ * @array: array to sort
+ * @size: size of array
+ * Return: void
  */
-int main(void)
+void radix_sort(int *array, size_t size)
 {
-    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
-    size_t n = sizeof(array) / sizeof(array[0]);
+    size_t i = 0;
+    size_t j = 0;
+    int k = 0;
+    int t = 0;
+    int min = 0;
+    int tmp = 0;
+    int *arr1 = malloc(size * sizeof(array));
 
-    print_array(array, n);
-    printf("\n");
-    radix_sort(array, n);
-    printf("\n");
-    print_array(array, n);
-    return (0);
+    for (i = 0; i < size; i++)
+        arr1[i] = array[i];
+
+    for (k = 0; k < 3; k++) {
+        for (i = 0; i < size; i++) {
+            min = array[i] % 10;
+            t = i;
+            for (j = i + 1; j < size; j++) {
+                if (min > (array[j] % 10)) {
+                    min = array[j] % 10;
+                    t = j;
+                }
+            }
+
+            tmp = arr1[t];
+            arr1[t] = arr1[i];
+            arr1[i] = tmp;
+
+            tmp = array[t];
+            array[t] = array[i];
+            array[i] = tmp;
+
+        }
+        for (j = 0; j < size; j++)
+            array[j] = array[j] / 10;
+
+        if (k < 2)
+            print_array(arr1, size);
+    }
+    for (i = 0; i < size; i++)
+        array[i] = arr1[i];
 }
